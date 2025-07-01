fc9c75b1967b9d084f03479c459677690ee23007
Lee KT <deltaforce91@naver.com>
2025-07-01 10:57:23 +0900

    리눅스 빌드시 스킵하도록 수정

--- Build/Build_Linux_arm64.bat
+++ Build/Build_Linux_arm64.bat
@@ -1,2 +1,4 @@
+@echo off
+set SKIP_VERSION_UPDATE=true
 dotnet build -r linux-arm64 "..\SeagullDiscordBot"
 pause
--- Build/Publish_Linux_arm64.bat
+++ Build/Publish_Linux_arm64.bat
@@ -1,2 +1,4 @@
+@echo off
+set SKIP_VERSION_UPDATE=true
 dotnet publish -r linux-arm64 "..\SeagullDiscordBot"
 pause
